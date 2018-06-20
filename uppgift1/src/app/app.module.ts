caca0bef77587d9d799fcf8e372c1577fa8a004d
JesperBengtsson <jeppebengtsson_@hotmail.com>
2018-06-20 12:58:35 +0200

    item and store c done

--- uppgift1/src/app/app.module.ts
+++ uppgift1/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { StoreComponent } from './store/store.component';
+import { ItemComponent } from './item/item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StoreComponent,
+    ItemComponent
   ],
   imports: [
     BrowserModule
